eb2cb2f566cee0063c0753297586c8630ca8b318
Arthur-10-coder <joseo@splunk.com>
2025-04-16 09:45:40 -0600

    04-16-2025 | Waiting for updates to continue with the license usage feature

--- package/bin/license_usage_helper.py
+++ package/bin/license_usage_helper.py
@@ -1,4 +1,3 @@
-
 import json
 import logging
 
@@ -7,19 +6,18 @@ from solnlib import conf_manager, log
 from solnlib import splunk_rest_client as rest_client
 from solnlib.modular_input import checkpointer
 from splunklib import modularinput as smi
-
-from datetime import datetime
+from datetime import datetime, timezone
+from time import sleep
 from genesyscloud_client import GenesysCloudClient
-from genesyscloud_models import EdgeModel
-
 
 ADDON_NAME = "genesys_cloud_ta"
 
 def logger_for_input(input_name: str) -> logging.Logger:
+    """Create and return a logger for the specified input."""
     return log.Logs().get_logger(f"{ADDON_NAME.lower()}_{input_name}")
 
-
 def get_account_property(session_key: str, account_name: str, property_name: str):
+    """Retrieve a specific property for a given Genesys Cloud account."""
     cfm = conf_manager.ConfManager(
         session_key,
         ADDON_NAME,
@@ -28,33 +26,29 @@ def get_account_property(session_key: str, account_name: str, property_name: str
     account_conf_file = cfm.get_conf("genesys_cloud_ta_account")
     return account_conf_file.get(account_name).get(property_name)
 
-
 def validate_input(definition: smi.ValidationDefinition):
+    """Validation function for the modular input (currently unused)."""
     return
 
-
 def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
-    # inputs.inputs is a Python dictionary object like:
-    # {
-    #   "edges_metrics://<input_name>": {
-    #     "account": "<account_name>",
-    #     "disabled": "0",
-    #     "host": "$decideOnStartup",
-    #     "index": "<index_name>",
-    #     "interval": "<interval_value>",
-    #     "python.version": "python3",
-    #   },
-    # }
+    """
+    Main function to retrieve Genesys Cloud conversation metrics,
+    process them, and write them to Splunk.
+    """
     for input_name, input_item in inputs.inputs.items():
         normalized_input_name = input_name.split("/")[-1]
         logger = logger_for_input(normalized_input_name)
         try:
             session_key = inputs.metadata["session_key"]
+
+            # Initialize KV store checkpointer
             kvstore_checkpointer = checkpointer.KVStoreCheckpointer(
-                "license_usage_checkpointer",
-                session_key,
-                ADDON_NAME,
+                 "license_usage_checkpointer",
+                  session_key,
+                  ADDON_NAME,
             )
+
+            # Set log level dynamically
             log_level = conf_manager.get_log_level(
                 logger=logger,
                 session_key=session_key,
@@ -62,85 +56,71 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
                 conf_name="genesys_cloud_ta_settings",
             )
             logger.setLevel(log_level)
+
             log.modular_input_start(logger, normalized_input_name)
+
+            # Retrieve account credentials with error handling
             account_region = get_account_property(session_key, input_item.get("account"), "region")
             client_id = get_account_property(session_key, input_item.get("account"), "client_id")
             client_secret = get_account_property(session_key, input_item.get("account"), "client_secret")
-            # aws_region = input_item.get('region')
 
-            client = GenesysCloudClient(
-                logger, client_id, client_secret, account_region
-            )
+            # Initialize Genesys Cloud client
+            client = GenesysCloudClient(logger, client_id, client_secret, account_region)
 
-            checkpointer_key_name = input_name.split("/")[-1]
-            # if we don't have any checkpoint, we default it to 1970
+            checkpointer_key_name = normalized_input_name
+            # Retrieve the last checkpoint or set it to 1970-01-01 if it doesn't exist
             current_checkpoint = (
-                kvstore_checkpointer.get(checkpointer_key_name)
-                or datetime(1970, 1, 1).timestamp()
+                kvstore_checkpointer.get(checkpointer_key_name) 
+                or  datetime(1970, 1, 1).timestamp()
             )
 
-            e_model = EdgeModel(client.get(
-                "TelephonyProvidersEdgeApi", "get_telephony_providers_edges")
-            )
+            start_time = datetime.fromtimestamp(current_checkpoint, tz=timezone.utc)
+            now = datetime.now(timezone.utc)
 
-            collection_name = "gc_edges"
-
-            service = rest_client.SplunkRestClient(session_key, ADDON_NAME)
-            if collection_name not in service.kvstore:
-                # Create collection
-                logger.debug(f"Creating lookup '{collection_name}'")
-                service.kvstore.create(collection_name)
-
-            # Update collection
-            logger.debug(f"Saving data in lookup '{collection_name}'")
-            collection = service.kvstore[collection_name]
-            collection.data.batch_save(*e_model.edges)
-
-            logger.debug("Indexing edges metrics")
-            # Max 100 edgeids supported according to specs
-            metrics = []
-            cnt = 0
-            has_more = True
-            while has_more:
-                edges_ids, has_more = e_model.get_edge_ids(cnt)
-                data = client.get(
-                    "TelephonyProvidersEdgeApi",
-                    "get_telephony_providers_edges_metrics",
-                    ','.join(edges_ids)
+            sourcetype = "genesyscloud:billing:billableusage"
+            while True:
+                # Perform API request
+                response = client.get(
+                    "BillingApi",
+                    "get_billing_reports_billableusage",
+                    start_date = str(start_time),
+                    end_data = str(now)
                 )
-                metrics.extend(data)
-                cnt+=1
-            logger.debug(f"Got '{len(metrics)}' metrics")
+                if response.to_dict().get('status') == "Complete":
+                    break 
+                else:
+                    sleep(1)
+                    
+            to_process_data = response.to_dict().get("usages", [])
 
-            sourcetype = "genesyscloud:telephonyprovidersedge:edges:metrics"
-            for metric_obj in metrics:
-                event_time_epoch = metric_obj.event_time.timestamp()
-                metric = metric_obj.to_dict()
-                metric["event_time"] = e_model.to_string(metric_obj.event_time)
-                if event_time_epoch > current_checkpoint:
-                    event_writer.write_event(
-                        smi.Event(
-                            data=json.dumps(metric, ensure_ascii=False, default=str),
-                            index=input_item.get("index"),
-                            sourcetype=sourcetype,
+            # Ensure data exists before processing
+            if to_process_data:
+                for event in to_process_data:
+                    try:
+                        event_writer.write_event(
+                            smi.Event(
+                                data=json.dumps(event, ensure_ascii=False, default=str),
+                                index=input_item.get("index"),
+                                sourcetype=sourcetype,
+                            )
                         )
-                    )
+                    except Exception as e:
+                        logger.error(f"Failed to write event: {str(e)}")
 
-            # Updating checkpoint if data was returned to avoid losing info
-            if metrics:
-                logger.debug("Updating checkpointer and leaving")
-                new_checkpoint = datetime.utcnow().timestamp()
-                kvstore_checkpointer.update(checkpointer_key_name, new_checkpoint)
+                # Only update checkpoint if data was processed
+                kvstore_checkpointer.update(checkpointer_key_name, now.timestamp())
 
-            log.events_ingested(
-                logger,
-                input_name,
-                sourcetype,
-                len(data),
-                input_item.get("index"),
-                account=input_item.get("account"),
-            )
+                log.events_ingested(
+                    logger,
+                    input_name,
+                    sourcetype,
+                    len(to_process_data),
+                    input_item.get("index"),
+                    account=input_item.get("account"),
+                )
+
             log.modular_input_end(logger, normalized_input_name)
+
         except Exception as e:
             log.log_exception(
                 logger,
